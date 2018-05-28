85d001228881c3b0961efaa3f9ab78b3978b8337
ken <309735651@qq.com>
2018-05-29 01:58:56 +0800

    ok

--- manage/urls.py
+++ manage/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^crm/', include("crm.urls")),
     url(r'^king_admin/', include("king_admin.urls")),
+    url(r'^myadmin/', include("myadmin.urls")),
 ]
