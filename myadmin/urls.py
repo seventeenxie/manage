cc4a969493ef7169b0b17171104b707eb07894be
ken <309735651@qq.com>
2018-05-31 00:37:17 +0800

    ok

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -5,4 +5,5 @@ from myadmin import views
 urlpatterns = [
     url(r'^$', views.index,name="myadmin/index"),
     url(r'table_index', views.table_index, name="table_index"),
+    url(r'^(\w+)/(\w+)/', views.display_table_objs,name="table_objs"),
 ]
