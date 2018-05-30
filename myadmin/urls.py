7d31b916c6dc2505cd4cf17d94503b2f9b805868
ken <309735651@qq.com>
2018-05-30 15:59:36 +0800

    ok

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -4,5 +4,5 @@ from myadmin import views
 
 urlpatterns = [
     url(r'^$', views.index,name="myadmin/index"),
-    url(r'table_index', views.table_index, name="table_index1"),
+    url(r'table_index', views.table_index, name="table_index"),
 ]
--- myadmin/views.py
+++ myadmin/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from myadmin import king_admin
 # Create your views here.
 def index(request):
     return  render(request,'myadmin/index.html')
+@csrf_exempt
 def table_index(request):
-    return render(request,'myadmin/table_index.html')
+    return render(request, "myadmin/table_index.html", {'table_list': king_admin.enabled_admins})
